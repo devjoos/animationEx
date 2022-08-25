32a3ed3cc2eeb7decc1d35836219c115b024b8ad
DevJoos <dev@dev.com>
2022-08-25 12:21:58 -0600

    adding custom animated transitions using ViewModifier

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -7,50 +7,89 @@
 
 import SwiftUI
 
+struct CornerRotateModifier: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
+    
+    func body(content: Content) -> some View {
+        content
+            .rotationEffect(.degrees(amount), anchor: anchor)
+            .clipped()
+    }
+}
+
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(
+            active: CornerRotateModifier(amount: -90, anchor: .topLeading),
+            identity: CornerRotateModifier(amount: 0, anchor: .topLeading)
+        )
+    }
+}
+
 struct ContentView: View {
     let letters = Array("Hello SwiftUI")
     @State private var enabled = false
     @State private var dragAmount = CGSize.zero
     @State private var isShowingRed = false
+    @State private var isShowingBlue = false
     
     var body: some View {
         VStack {
-            VStack {
-                Button("tap me") {
-                    withAnimation {
-                        isShowingRed.toggle()
-                    }
-                }
-                if isShowingRed {
+            ZStack {
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 200, height: 200)
+                
+                if isShowingBlue {
                     Rectangle()
-                        .fill(.red)
+                        .fill(.blue)
                         .frame(width: 200, height: 200)
-                        .transition(.asymmetric(insertion: .scale, removal: .opacity))
+                        .transition(.pivot)
                 }
             }
-            HStack(spacing: 0) {
-                ForEach(0..<letters.count, id: \.self) {
-                    Text(String(letters[$0]))
-                        .padding(5)
-                        .font(.title)
-                        .background(enabled ? .blue : .red)
-                        .offset(dragAmount)
-                        .animation(.default.delay(Double($0) / 20), value: dragAmount)
+            .onTapGesture {
+                withAnimation {
+                    isShowingBlue.toggle()
                 }
-            }
-            .gesture(
-                DragGesture()
-                    .onChanged { dragAmount = $0.translation}
-                    .onEnded { _ in dragAmount = .zero
-                        enabled.toggle()
-                    }
-            )
+            
         }
-        
-        
-        
-        
+        VStack {
+            Button("tap me") {
+                withAnimation {
+                    isShowingRed.toggle()
+                }
+            }
+            if isShowingRed {
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
+            }
+        }
+        HStack(spacing: 0) {
+            ForEach(0..<letters.count, id: \.self) {
+                Text(String(letters[$0]))
+                    .padding(5)
+                    .font(.title)
+                    .background(enabled ? .blue : .red)
+                    .offset(dragAmount)
+                    .animation(.default.delay(Double($0) / 20), value: dragAmount)
+            }
+        }
+        .gesture(
+            DragGesture()
+                .onChanged { dragAmount = $0.translation}
+                .onEnded { _ in dragAmount = .zero
+                    enabled.toggle()
+                }
+        )
     }
+    
+    
+    
+    
+}
 }
 
 
