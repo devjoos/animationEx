8533ff8128f1fd365befc58055b1f5d5c81ec76a
DevJoos <dev@dev.com>
2022-08-25 10:33:18 -0600

    Showing dragGestrures with a delayed animation

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -8,11 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    let letters = Array("Hello SwiftUI")
+    @State private var enabled = false
+    @State private var dragAmount = CGSize.zero
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack(spacing: 0) {
+            ForEach(0..<letters.count, id: \.self) {
+                Text(String(letters[$0]))
+                    .padding(5)
+                    .font(.title)
+                    .background(enabled ? .blue : .red)
+                    .offset(dragAmount)
+                    .animation(.default.delay(Double($0) / 20), value: dragAmount)
+            }
+        }
+        .gesture(
+            DragGesture()
+                .onChanged { dragAmount = $0.translation}
+                .onEnded { _ in dragAmount = .zero
+                    enabled.toggle()
+                }
+        )
+
+     
+            
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
